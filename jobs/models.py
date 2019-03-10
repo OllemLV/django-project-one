a0fc5201441539bdb46449455afa611369f2f012
OllemLV <ollemlv@ukr.net>
2019-03-10 19:57:15 +0200

    Bootstrap and Jobs view are ok

--- jobs/models.py
+++ jobs/models.py
@@ -1,9 +1,5 @@
 from django.db import models
 
 class Job(models.Model):
-    title = models.CharField(max_length=256)
-    start_date = models.DateField(blank=True)
-    finish_date = models.DateField(blank=True)
     description = models.TextField()
-    additional_link = models.URLField(max_length=256, blank=True)
     image = models.ImageField(upload_to='images/')
