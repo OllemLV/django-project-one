3d629a2b0a8363907e59db9afff5a4a2771bba16
OllemLV <ollemlv@ukr.net>
2019-03-10 16:15:51 +0200

    Switch to PostgreSQL

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Job(models.Model):
+    title = models.CharField(max_length=256)
+    start_date = models.DateField(blank=True)
+    finish_date = models.DateField(blank=True)
+    description = models.TextField()
+    additional_link = models.URLField(max_length=256, blank=True)
+    image = models.ImageField(upload_to='images/')
